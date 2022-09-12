8c89ef3f7999654eee34c9b42087ff4ccfae4793
TrevorTress <trevor.tress@outlook.com>
2022-09-12 19:15:21 -0400

    added action and move select functionality

--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState {Start, PlayerAction, PlayerMove, EnemyMove, Busy}
 
 public class BattleSystem : MonoBehaviour
 {
@@ -12,12 +15,16 @@ public class BattleSystem : MonoBehaviour
 
     [SerializeField] BattleDialogBox dialogBox;
 
+    BattleState state;
+    int currentAction;
+    int currentMove;
+
     private void Start()
     {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle()
+    public IEnumerator SetupBattle()
     {
         playerUnit.Setup();
         playerHud.SetData(playerUnit.Comic);
@@ -25,6 +32,109 @@ public class BattleSystem : MonoBehaviour
         enemyUnit.Setup();
         enemyHud.SetData(enemyUnit.Comic);
 
-        StartCoroutine(dialogBox.TypeDialog($"A wild {playerUnit.Comic.Base.Name} appeared."));
+        dialogBox.SetMoveNames(playerUnit.Comic.Moves);
+
+        yield return dialogBox.TypeDialog($"A wild {enemyUnit.Comic.Base.Name} appeared.");
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
+    }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    void PlayerMove()
+    {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+
+    private void Update()
+    {
+        if (state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+        else if (state == BattleState.PlayerMove)
+        {
+            HandleMoveSelection();
+        }
+    }
+    void HandleActionSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(currentAction < 3)
+                ++currentAction;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(currentAction > 0)
+                --currentAction;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(currentAction < 2)
+                currentAction += 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if(currentAction > 1)
+                currentAction -= 2;
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+                PlayerMove();
+            }
+            else if (currentAction == 1)
+            {
+                // comics
+            }
+            else if (currentAction == 2)
+            {
+                // bag
+            }
+            else if (currentAction == 3)
+            {
+                // run
+            }
+        }
+    }
+
+    void HandleMoveSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(currentMove < playerUnit.Comic.Moves.Count - 1)
+                ++currentMove;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(currentMove > 0)
+                --currentMove;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(currentMove < playerUnit.Comic.Moves.Count - 2)
+                currentMove += 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if(currentMove > 1)
+                currentMove -= 2;
+        }
+
+        dialogBox.UpdateMoveSelection(currentMove, playerUnit.Comic.Moves[currentMove]);
     }
 }
