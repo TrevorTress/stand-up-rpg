dad9e42fde02ffb11ee46da5fb44d74c27b3ae4e
TrevorTress <trevor.tress@outlook.com>
2022-09-20 13:28:29 -0400

    walk bug fixed

--- Assets/Scripts/Character/NPCController.cs
+++ Assets/Scripts/Character/NPCController.cs
@@ -21,13 +21,19 @@ public class NPCController : MonoBehaviour, Interactable
     public void Interact()
     {
         if (state == NPCState.Idle)
-            StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
+        {
+            state = NPCState.Dialog;
+            StartCoroutine(DialogManager.Instance.ShowDialog(dialog, () => {
+                idleTimer = 0f;
+                state = NPCState.Idle;
+            }));
         //StartCoroutine(character.Move(new Vector2(2,0)));
+        }
+            
     }
 
     private void Update()
     {
-        if (DialogManager.Instance.IsShowing) return;
         if (state == NPCState.Idle)
         {
             idleTimer += Time.deltaTime;
@@ -52,4 +58,4 @@ public class NPCController : MonoBehaviour, Interactable
     }
 }
 
-public enum NPCState { Idle, Walking }
+public enum NPCState { Idle, Walking, Dialog }
