e81f5d4f4f02b7b9647911b0f05d18753c7f059f
TrevorTress <trevor.tress@outlook.com>
2022-09-20 11:28:23 -0400

    character class refactored, char animation added

--- Assets/Scripts/Character/NPCController.cs
+++ Assets/Scripts/Character/NPCController.cs
@@ -5,8 +5,35 @@ using UnityEngine;
 public class NPCController : MonoBehaviour, Interactable
 {
     [SerializeField] Dialog dialog;
+
+    NPCState state;
+    float idleTimer = 0f;
+
+    Character character;
+
+    private void Awake()
+    {
+        character = GetComponent<Character>();
+    }
     public void Interact()
     {
-        StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
+        //StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
+        //StartCoroutine(character.Move(new Vector2(2,0)));
+    }
+
+    private void Update()
+    {
+        if (state == NPCState.Idle)
+        {
+            idleTimer += Time.deltaTime;
+            if (idleTimer > 2f)
+            {
+                idleTimer = 0f;
+                StartCoroutine(character.Move(new Vector2(1,0)));
+            }
+        }
+        character.HandleUpdate();
     }
 }
+
+public enum NPCState { Idle, Walking }
--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -6,30 +6,21 @@ using UnityEngine;
 // controls actions of player
 public class PlayerController : MonoBehaviour
 {
-    // create references to different map interactions
-    public float moveSpeed;
-    public LayerMask solidObjectsLayer;
-    public LayerMask interactableLayer;
-    public LayerMask grassLayer;
     public event Action OnEncounter;
-
-    // keep track of player movement and input
-    private bool isMoving;
     private Vector2 input;
 
-    // reference to unity animator
-    private CharacterAnimator animator;
+    private Character character;
 
     private void Awake()
     {
-        animator = GetComponent<CharacterAnimator>();
+        character = GetComponent<Character>();
     }
 
     // movement system
     public void HandleUpdate()
     {
         // if player isnt actively moving 
-        if (!isMoving)
+        if (!character.IsMoving)
         {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
@@ -40,21 +31,11 @@ public class PlayerController : MonoBehaviour
 
             if (input != Vector2.zero)
             {
-                animator.MoveX = input.x;
-                animator.MoveY = input.y;
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
-
-                // if target passes walkable check
-                if (IsWalkable(targetPos))
-                    // initiate movement function
-                    StartCoroutine(Move(targetPos));
+                StartCoroutine(character.Move(input, CheckForBattle));
             }
         }
 
-        // tell animator character is moving
-        animator.IsMoving = isMoving;
+        character.HandleUpdate();
 
         if (Input.GetKeyDown(KeyCode.Z))
             Interact();
@@ -62,53 +43,26 @@ public class PlayerController : MonoBehaviour
 
     void Interact()
     {
-        var facingDir = new Vector3(animator.MoveX, animator.MoveY);
+        var facingDir = new Vector3(character.Animator.MoveX, character.Animator.MoveY);
         var interactPos = transform.position + facingDir;
 
-        var collider = Physics2D.OverlapCircle(interactPos, 0.3f, interactableLayer);
+        var collider = Physics2D.OverlapCircle(interactPos, 0.3f, GameLayers.i.InteractableLayer);
         if (collider != null)
         {
             collider.GetComponent<Interactable>()?.Interact();
         }
     }
 
-    // movement function that checks for grass/battle at the end
-    IEnumerator Move(Vector3 targetPos)
-    {
-        isMoving = true;
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        transform.position = targetPos;
-        isMoving = false;
-
-        CheckForBattle();
-    }
-
-    // checks if terrain is walkable
-    private bool IsWalkable(Vector3 targetPos)
-    {
-        // if target position collides with solidObjectsLayer
-        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer | interactableLayer) != null)
-        {
-            // IsWalkable() returns false
-            return false;
-        }
-        return true;
-    }
-
     // function for initiating battle in grass
     private void CheckForBattle()
     {
         // if player is colliding with grass tile during movement
-        if (Physics2D.OverlapCircle(transform.position, 0.1f, grassLayer) != null)
+        if (Physics2D.OverlapCircle(transform.position, 0.1f, GameLayers.i.GrassLayer) != null)
         {
             // 10% chance to initialize combat
             if (UnityEngine.Random.Range(1, 101) <= 10)
             {
-                animator.IsMoving = false;
+                character.Animator.IsMoving = false;
                 OnEncounter();
             }
         }
